981c14f75e6f1c03d7790884eabbf0e85b68e9c4
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-10-15 20:57:39 -0700

    Add getInitialState method

--- process/js/app.js
+++ process/js/app.js
@@ -2,11 +2,27 @@ var React = require('react');
 var ReactDom = require('react-dom');
 
 var MainInterface = React.createClass({
+
+  getInitialState: function() {
+    return {
+      title: 'Appointments',
+      show: true
+    } // return
+  }, // getInitialState
+
   render: function() {
     return (
-      <h1>Student Appointments</h1>
-    )
+      <div className="interface">
+        <h1>{ this.state.show ? 'Student ' : null }
+        { this.state.title }</h1>
+        <ul>
+          <li>Jane Doe 2:00 PM</li>
+          <li>John Smith 3:00 PM</li>
+        </ul>
+      </div>
+    ) // return
   } // render
+
 }); // MainInterface
 
 ReactDom.render(
