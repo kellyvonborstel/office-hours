daacff26087951d91ea203e2fd058a33e1c81534
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-10-15 23:32:16 -0700

    Add AppointmentList component

--- process/js/app.js
+++ process/js/app.js
@@ -5,28 +5,30 @@ var MainInterface = React.createClass({
 
   getInitialState: function() {
     return {
-      myAppointmens: []
+      myAppointments: []
     } // return
   }, // getInitialState
+
+  componentDidMount: function() {
+    this.serverRequest = $.get('./js/data.json', function(result) {
+      var tempAppointments = result;
+      this.setState({
+        myAppointments: tempAppointments
+      }); // setState
+    }.bind(this)); //tempAppointments
+  }, // componentDidMount
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
 
   render: function() {
     var filteredAppointments = this.state.myAppointments;
 
     filteredAppointments = filteredAppointments.map(function(item, index) {
       return (
-        <li className="appointment-item media" key={ index }>
-          <div className="appointment-info media-body">
-            <div className="appointment-head">
-              <span className="student-name">{ this.state.myAppointments[index].studentName }</span>
-              <span className="appointment-date pull-right">{ this.state.myAppointments[index].appointmentDate }</span>
-            </div>
-          </div>
-          <div className="course-name">
-            <span className="label-item">Course: </span>
-            { this.state.myAppointments[index].courseName }
-          </div>
-          <div className="appointment-notes">{ this.state.myAppointments[index].appointmentNotes }</div>
-        </li>
+        <AppointmentList key = { index }
+          singleItem = { item } />
       ) // return
     }.bind(this)); //filteredAppointments
 
@@ -38,6 +40,26 @@ var MainInterface = React.createClass({
   } // render
 
 }); // MainInterface
+
+var AppointmentList = React.createClass({
+  render: function() {
+    return(
+      <li className="appointment-item media">
+        <div className="appointment-info media-body">
+          <div className="appointment-head">
+            <span className="student-name">{ this.props.singleItem.studentName }</span>
+            <span className="appointment-date pull-right">{ this.props.singleItem.appointmentDate }</span>
+          </div>
+        </div>
+        <div className="course-name">
+          <span className="label-item">Course: </span>
+          { this.props.singleItem.courseName }
+        </div>
+        <div className="appointment-notes">{ this.props.singleItem.appointmentNotes }</div>
+      </li>
+    ) // return
+  } // render
+}); // AppointmentList
 
 ReactDom.render(
   <MainInterface />,
