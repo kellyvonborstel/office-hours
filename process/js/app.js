973a9f6bfa29e82f0766c48e0be0691d6a0fc7cf
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-10-15 22:29:27 -0700

    Move data into json file

--- process/js/app.js
+++ process/js/app.js
@@ -5,52 +5,27 @@ var MainInterface = React.createClass({
 
   getInitialState: function() {
     return {
-      data: [
-        {
-          "studentName": "Jane Doe",
-          "courseName": "VC 237",
-          "appointmentDate": "2016-12-10 15:30",
-          "appointmentNotes": "Discuss exam grade and extra credit work"
-        },
-        {
-          "studentName": "John Smith",
-          "courseName": "VC 238",
-          "appointmentDate": "2016-12-10 14:30",
-          "appointmentNotes": "Go over forms assignment"
-        },
-        {
-          "studentName": "Eric Jones",
-          "courseName": "VC 237",
-          "appointmentDate": "2016-12-12 15:30",
-          "appointmentNotes": "Discuss project deadline extension"
-        },
-        {
-          "studentName": "Emily Brown",
-          "courseName": "VC 238",
-          "appointmentDate": "2016-12-12 16:00",
-          "appointmentNotes": "Review material from recent absence"
-        }
-      ]
+      myAppointmens: []
     } // return
   }, // getInitialState
 
   render: function() {
-    var filteredAppointments = this.state.data;
+    var filteredAppointments = this.state.myAppointments;
 
     filteredAppointments = filteredAppointments.map(function(item, index) {
       return (
         <li className="appointment-item media" key={ index }>
           <div className="appointment-info media-body">
             <div className="appointment-head">
-              <span className="student-name">{ this.state.data[index].studentName }</span>
-              <span className="appointment-date pull-right">{ this.state.data[index].appointmentDate }</span>
+              <span className="student-name">{ this.state.myAppointments[index].studentName }</span>
+              <span className="appointment-date pull-right">{ this.state.myAppointments[index].appointmentDate }</span>
             </div>
           </div>
           <div className="course-name">
             <span className="label-item">Course: </span>
-            { this.state.data[index].courseName }
+            { this.state.myAppointments[index].courseName }
           </div>
-          <div className="appointment-notes">{ this.state.data[index].appointmentNotes }</div>
+          <div className="appointment-notes">{ this.state.myAppointments[index].appointmentNotes }</div>
         </li>
       ) // return
     }.bind(this)); //filteredAppointments
